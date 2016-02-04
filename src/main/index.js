09d10bc56411d103b897201b48e5b6157e0532a0
Graham Cox <graham@grahamcox.co.uk>
2016-02-04 13:27:25 +0000

    Added in support for the tester to start up a server and make requests against it. It's a bit clunky but it does work

--- src/main/index.js
+++ src/main/index.js
@@ -1,34 +1,52 @@
 import request from 'request';
+import freeport from 'freeport';
 
 export function tester({
     url,
+    server,
     method = 'POST',
     contentType = 'application/graphql'
 }) {
     return (query) => {
         return new Promise((resolve, reject) => {
-            request({
-                method,
-                uri: url,
-                headers: {
-                    'Content-Type': contentType
-                },
-                body: query
-            }, (error, message, body) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    const result = JSON.parse(body);
-                    
-                    resolve({
-                        raw: body,
-                        data: result.data,
-                        errors: result.errors,
-                        headers: message.headers,
-                        status: message.statusCode,
-                        success: !result.hasOwnProperty('errors')
-                    });
-                }
+            if (server) {
+                freeport((err, port) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(server.creator(port)
+                            .then((baseUrl) => baseUrl + url));
+                    }
+                });
+            } else {
+                resolve(url);
+            }
+        }).then((realUrl) => {
+            console.log(`Sending query ${query} to ${realUrl}`);
+            return new Promise((resolve, reject) => {
+                request({
+                    method,
+                    uri: realUrl,
+                    headers: {
+                        'Content-Type': contentType
+                    },
+                    body: query
+                }, (error, message, body) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        const result = JSON.parse(body);
+                        
+                        resolve({
+                            raw: body,
+                            data: result.data,
+                            errors: result.errors,
+                            headers: message.headers,
+                            status: message.statusCode,
+                            success: !result.hasOwnProperty('errors')
+                        });
+                    }
+                });
             });
         });
     };
--- src/test/servers/hapi-server.test.js
+++ src/test/servers/hapi-server.test.js
@@ -4,30 +4,39 @@ import {TestSchema} from './schema';
 import {tester} from '../../main';
 
 describe('Hapi Server', () => {
-    const server = new Hapi.Server();
-
-    server.register({
-        register: GraphQL,
-        options: {
-            query: {
-              schema: TestSchema,
-              rootValue: {},
-              pretty: false
-            },
-            route: {
-              path: '/graphql',
-              config: {}
+    let HapiTest = tester({
+        server: {
+            creator: (port) => {
+                const server = new Hapi.Server();
+                server.connection({
+                    port
+                });
+                
+                return server.register([
+                    {
+                        register: GraphQL,
+                        options: {
+                            query: {
+                              schema: TestSchema,
+                              rootValue: {},
+                              pretty: false
+                            },
+                            route: {
+                              path: '/graphql',
+                              config: {}
+                            }
+                        }
+                    }
+                ])
+                .then(() => server.start())
+                .then(() => server.info.uri);
             }
-        }
-    });
-    
-    let InvalidHostnameTest = tester({
-        server: server,
+        },
         url: '/graphql'
     });
     
     describe('Valid Query', () => {
-        const response = InvalidHostnameTest('{ test(who: "Graham") }');
+        const response = HapiTest('{ test(who: "Graham") }');
         
         it('Returns success', () => {
             return response.should.eventually.have.property('success').equal(true);
@@ -36,7 +45,21 @@ describe('Hapi Server', () => {
             return response.should.eventually.have.property('status').equal(200);
         });
         it('Returns the correct name', () => {
-            return response.should.eventually.have.deep.property('data').equal('Hello Graham');
+            return response.should.eventually.have.deep.property('data.test').equal('Hello Graham');
+        });
+    });
+    
+    describe('Invalid Query', () => {
+        const response = HapiTest('{ somethingElse(who: "Graham") }');
+        
+        it('Returns success', () => {
+            return response.should.eventually.have.property('success').equal(false);
+        });
+        it('Returns the correct Status code', () => {
+            return response.should.eventually.have.property('status').equal(400);
+        });
+        it('Returns some errors', () => {
+            return response.should.eventually.have.property('errors');
         });
     });
 });
