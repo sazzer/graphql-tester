2a32ad20a25aea2cd14778d262a8c0c35349da2c
Graham Cox <graham@grahamcox.co.uk>
2016-02-05 19:11:52 +0000

    Added test for running an Express Server with GraphQL support

---
+++ src/test/servers/express-server.test.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import GraphQL from 'express-graphql';
+import {TestSchema} from './schema';
+import {tester} from '../../main';
+
+describe('Express Server', () => {
+    let ExpressTest = tester({
+        server: {
+            creator: (port) => {
+                const server = express();
+                server.use('/graphql', GraphQL({
+                    schema: TestSchema
+                }));
+
+                return new Promise((resolve, reject) => {
+                    server.listen(port, () => {
+                        resolve(`http://localhost:${port}`);
+                    });
+                });
+            }
+        },
+        url: '/graphql'
+    });
+
+    describe('Valid Query', () => {
+        const response = ExpressTest('{ test(who: "Graham") }');
+
+        it('Returns success', () => {
+            return response.should.eventually.have.property('success').equal(true);
+        });
+        it('Returns the correct Status code', () => {
+            return response.should.eventually.have.property('status').equal(200);
+        });
+        it('Returns the correct name', () => {
+            return response.should.eventually.have.deep.property('data.test').equal('Hello Graham');
+        });
+    });
+
+    describe('Invalid Query', () => {
+        const response = ExpressTest('{ somethingElse(who: "Graham") }');
+
+        it('Returns success', () => {
+            return response.should.eventually.have.property('success').equal(false);
+        });
+        it('Returns the correct Status code', () => {
+            return response.should.eventually.have.property('status').equal(400);
+        });
+        it('Returns some errors', () => {
+            return response.should.eventually.have.property('errors');
+        });
+    });
+});
